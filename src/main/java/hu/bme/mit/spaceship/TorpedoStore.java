3287e395c5421b2f8093f499c160560531af66d2
Gajdátsy Márk Levente <mark@gajdatsy.hu>
2024-10-04 13:22:23 +0200

    added comment to th placement of the random generator

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -8,7 +8,7 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-  private Random generator = new Random();
+  private Random generator = new Random(); //to ensure proper randomness
     
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
